cc51cae0155215baec4e7407a0176c0d537490b7
Elvar Pétur Indriðason <elvarpi@gmail.com>
2017-04-01 13:53:27 +0000

    Update readme file and change to cronjob to run every hour

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-# IceLeague
+# Hvað er þetta?
 
-Kóðinn fyrir vefsíðuna http://iceleague.is
+IceLeague er vefsíða sem sýnir upplýsingar um Íslenska League of Legends spilara með gögnum í gegnum forritunarviðmótið (API) hjá Riot Games.
 
-Vefsíðan rankar íslenska spilara sem skrá sig með stats frá riot api
+Link á síðu: http://iceleague.is
 
 ## Getting Started
 
@@ -12,14 +12,49 @@ Verður að vera með API key frá Riot Games https://developer.riotgames.com/
 
 ## Built With
 
-* [Bootstrap](http://getbootstrap.com/) - Framework (theme frá [Bootswatch](https://bootswatch.com/flatly/))
-* [DataTables](datatables) - Plugin fyrir tables (sorting, searching o.fl.)
+* [Bootstrap](https://getbootstrap.com/) - Framework (theme frá [Bootswatch](https://bootswatch.com/flatly/))
+* [DataTables](datatables) - jQuery plugin fyrir tables (sorting, searching o.fl.)
 * [LeagueWrap](https://github.com/LeaguePHP/LeagueWrap) - League of legends API wrapper
 
 ## Contributing
 
-Endilega gerðu pull request þegar þú sérð eitthvað sem má laga eða betrumbæta (sem þú munnt eflaust gera!)
+Ef þú sérð eitthvað sem má betrumbæta eða sem þarf að laga þá máttu endilega gerðu pull request.
+
+Nokkrar hugmyndir sem má hægt er að bæta við síðuna (at the top of my head)
+
++ Listi yfir Íslenska streamara
++ Match history fyrir spilara
++ Detailed stats þegar maður klikkar á spilara
++ Safna fleiri stats um spilara (Akkurat nuna safnar síðan bara Ranked stats, League stats, og Champion Mastery Stats)
++ Safna upplýsingum um notendur í rauntíma í staðinn fyrir að nota cronjob á klukkutíma fresti
++ Bæta við Cronjob Logs
+
+Ástæðan afhverju ég er ekki buinn að bæta þessu við flestum af þessum hugmyndum er vegna þess að þá er síðan orðinn mjög svipuð öðrum síðun eins og [op.gg](https://op.gg/) og [lolking.net](https://lolking.net) sem var ekki tilgangurinn að síðunni.
+
+## Known bugs
+
+Ekkert sem ég veit af í augnablikinu.
 
 ## License
 
-This project is licensed under the GNU LESSER GENERAL PUBLIC LICENSE - see the [LICENSE.md](LICENSE.md) file for details
+MIT License
+
+Copyright (c) 2017 IceLeague
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
--- public_html/faq.php
+++ public_html/faq.php
@@ -7,36 +7,12 @@
     <div class="panel panel-default">
       <div class="panel-heading">
         <h4 class="panel-title">
-          <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapse1">Hvað er IceLeague?</a>
+          <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapse1">Hvað er IceLeague og hvað er tilgangurinn með síðunni?</a>
         </h4>
       </div>
       <div id="collapse1" class="panel-collapse collapse in">
         <div class="panel-body">
-          IceLeague er listi yfir alla íslenska League Of Legends spilara.
-        </div>
-      </div>
-    </div>
-    <div class="panel panel-default">
-      <div class="panel-heading">
-        <h4 class="panel-title">
-          <a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapse2">Hver er tilgangurinn með IceLeague?</a>
-        </h4>
-      </div>
-      <div id="collapse2" class="panel-collapse collapse">
-        <div class="panel-body">
-          Tilgangurinn með Iceleague er að auðvelda yfirsýn yfir íslenska spilara.
-        </div>
-      </div>
-    </div>
-    <div class="panel panel-default">
-      <div class="panel-heading">
-        <h4 class="panel-title">
-          <a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapse3">Afhverju ætti ég að skrá mig á IceLeague?</a>
-        </h4>
-      </div>
-      <div id="collapse3" class="panel-collapse collapse">
-        <div class="panel-body">
-          Svo það sé auðveldara fyrir aðra Íslendinga að finna þig, eins og t.d. til þess að búa til lið.
+          <p>IceLeague er listi yfir alla íslenska League Of Legends spilara. Tilgangurinn með síðuna er að auðvelda yfirsýn yfir íslenska spilara, eins og t.d til þess að búa til lið.</p>
         </div>
       </div>
     </div>
@@ -48,7 +24,7 @@
       </div>
       <div id="collapse4" class="panel-collapse collapse">
         <div class="panel-body">
-          Listarnir uppfærast á 24 tíma fresti.
+          <p>Listarnir uppfærast á klukkutíma fresti.</p>
         </div>
       </div>
     </div>
@@ -60,7 +36,7 @@
       </div>
       <div id="collapse5" class="panel-collapse collapse">
         <div class="panel-body">
-          Sendu mér email (elvarpi(hjá)gmail.com) með summoner nafninu þínu og ég skal finna út úr því.
+          <p>Sendu mér email (elvarpi(hjá)gmail.com) með summoner nafninu þínu og ég skal finna út úr því.</p>
         </div>
       </div>
     </div>
@@ -72,7 +48,7 @@
       </div>
       <div id="collapse6" class="panel-collapse collapse">
         <div class="panel-body">
-          Summoner Nafn: Álfasteinn
+          <p>Ég heiti Elvar Pétur gaman að kynnast þér :), Nafnið mitt í League er Álfasteinn á EUNE</p>
         </div>
       </div>
     </div>
@@ -84,7 +60,7 @@
       </div>
       <div id="collapse7" class="panel-collapse collapse">
         <div class="panel-body">
-          Já! Emailið er: elvarpi(hjá)gmail.com
+          <p>Já! Emailið er: elvarpi(hjá)gmail.com</p>
         </div>
       </div>
     </div>
@@ -96,7 +72,7 @@
       </div>
       <div id="collapse8" class="panel-collapse collapse">
         <div class="panel-body">
-          Já! Kóðinn er á Github: <a href="https://github.com/ElvarP/IceLeague">https://github.com/ElvarP/IceLeague</a>
+          <p>Já! Kóðinn er á Github: <a href="https://github.com/ElvarP/IceLeague">https://github.com/ElvarP/IceLeague</a></p>
         </div>
       </div>
     </div>
@@ -108,10 +84,10 @@
       </div>
       <div id="collapse9" class="panel-collapse collapse">
         <div class="panel-body">
-          Já, ef þú vilt það!
+          <p>Já, ef þú vilt það!</p>
           <br>
           <br>
-          Allur peningur fer í að halda síðunni uppi og til þess að bæta við nýjum eiginleikum (þú getur t.d. sagt mér í donate message hvaða eiginlega þér langar ég bæti við síðuna!)
+          <p>Allur peningur fer í að halda síðunni uppi og til þess að bæta við nýjum eiginleikum (þú getur t.d. sagt mér í donate message hvaða eiginlega þér langar ég bæti við síðuna!)</p>
           <br>
           <br>
           <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
@@ -131,9 +107,9 @@
       </div>
       <div id="collapse10" class="panel-collapse collapse">
         <div class="panel-body">
-          IceLeague er ekki styrkt af Riot Games og endurspeglar ekki skoðanir hjá Riot Games eða einhvern sem opinberlega tekur þátt í að framleiða League Of Legends.
+          IceLeague er ekki styrkt af Riot Games og endurspeglar ekki skoðanir hjá Riot Games eða einhvern sem opinberlega tekur þátt í að framleiða League Of Legends.</p>
           <br>
-          League of Legends og Riot Games er skrásett vörumerki hjá Riot Games, Inc. League of legends © Riot Games, Inc.
+          League of Legends og Riot Games er skrásett vörumerki hjá Riot Games, Inc. League of legends © Riot Games, Inc.</p>
         </div>
       </div>
     </div>

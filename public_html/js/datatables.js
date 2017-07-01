4b746a03434b672da199a5a8aada2a1d336ccd9c
elvarp <elvarpi@gmail.com>
2017-07-01 16:14:54 +0000

    create more modular javascript

---
+++ public_html/js/datatables.js
@@ -0,0 +1,59 @@
+//Loada Datatables fyrir tables
+$(document).ready(function() {
+  $('.leagues').dataTable( {
+    "language": {
+      "url": "//cdn.datatables.net/plug-ins/1.10.7/i18n/Icelandic.json"
+    },
+    "iDisplayLength": 15,
+    "lengthMenu": [10, 15, 25, 50, 100],
+    //Raða DIVISION eftir RANK í LEAGUES table
+    columnDefs: [
+      {
+        targets: [2],
+        orderData: [0]
+      }
+    ],
+    responsive: true,
+  });
+  $('.rankedstats').dataTable( {
+    "language": {
+      "url": "//cdn.datatables.net/plug-ins/1.10.7/i18n/Icelandic.json"
+    },
+    "iDisplayLength": 15,
+    "lengthMenu": [10, 15, 25, 50, 100],
+    responsive: true,
+  });
+  $('.championmastery').dataTable( {
+    "language": {
+      "url": "//cdn.datatables.net/plug-ins/1.10.7/i18n/Icelandic.json"
+    },
+    "iDisplayLength": 15,
+    "lengthMenu": [10, 15, 25, 50, 100],
+    responsive: true,
+  });
+  //Bæta við Index (númer) við Ranked stats og Champion Mastery tables
+  $('.rankedstats').on( 'draw.dt', function () {
+    $('tr').each(function (i) {
+      $("td:first", this).html(i);
+    });
+  });
+  $('.championmastery').on( 'draw.dt', function () {
+    $('tr').each(function (i) {
+      $("td:first", this).html(i);
+    });
+  });
+});
+//Breyta liti á KDA eftir hversu hátt KDA-ið er
+$('.kda').each(function() {
+  var $this = $(this);
+  var value = $this.text();
+  if(value >= 2) {
+    $this.addClass('kda_green');
+  }
+  if (value >= 3) {
+    $this.addClass('kda_blue');
+  }
+  if (value >= 4) {
+    $this.addClass('kda_orange');
+  }
+});

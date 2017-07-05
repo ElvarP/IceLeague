0956ff95d7dee3672784165b421272d5034779cc
elvarp <elvarpi@gmail.com>
2017-07-05 21:36:54 +0000

    remove footer copyright notice

--- public_html/footer.php
+++ public_html/footer.php
@@ -2,32 +2,5 @@
 <script type="text/javascript" src="js/datatables.js"></script>
 <script type="text/javascript" src="js/forms.js"></script>
 <script type="text/javascript" src="js/analytics.js"></script>
-<style media="screen">
-.footer {
-
-}
-body {
-  min-height: 100vh;
-  position: relative;
-  margin: 0;
-}
-
-footer {
-  position: absolute;
-  bottom: 0;
-  width: 100%;
-  /* Set the fixed height of the footer here */
-  height: 60px;
-  background-color: #f5f5f5;
-}
-footer .text-muted {
-    margin: 20px 0;
-}
-</style>
-<footer class="footer">
-   <div class="container">
-     <p class="text-muted">Place sticky footer content here.</p>
-   </div>
- </footer>
 </body>
 </html>

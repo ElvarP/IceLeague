ac7d02531ef6e6795b286cc6ead5e0f0fdff5e49
elvarp <elvarpi@gmail.com>
2017-07-12 07:16:31 +0000

    edit config file

--- config-sample.php
+++ config-sample.php
@@ -1,24 +1,17 @@
 <?php
-
 // API KEYS frá riot games https://developer.riotgames.com/
-if ($_SERVER['HTTP_HOST'] == 'localhost')
-{
-  // DEVELOPMENT API Key
-  $key = 'xxxx-xxxx-xxxx-xxxx';
+define('ENVIRONMENT', 'PRODUCTION');
+if (ENVIRONMENT == 'DEVELOPMENT') {
+  $key = 'xxxx'; //DEVELOPMENT API KEY
 }
-else
-{
-  // PRODUCTION API Key
-  $key = 'xxxx-xxxx-xxxx-xxxx';
+elseif (ENVIRONMENT == 'PRODUCTION') {
+  $key = 'xxxx'; //PRODUCTION API KEY
 }
-
 
 ## League of legends servers og ranked queues sem að vefsíðan styður í augnablikinu.
 ## Það er hægt að bæta við fleirum, en þá verður að breyta SQL skjalinu (Bæta við tables fyrir aðra servera)
 $supported_league_servers = ['eune', 'euw', 'na'];
 $supported_ranked_queues = ['ranked_solo_5x5', 'ranked_flex_sr', 'ranked_flex_tt'];
-
-
 
 ## Þetta er version-ið af data dragon
 ## Ég nota data dragon til að sýna myndir af hetjum í champion_mastery.php flipanum
@@ -27,15 +20,13 @@ $supported_ranked_queues = ['ranked_solo_5x5', 'ranked_flex_sr', 'ranked_flex_tt
 ## Í augnblikinu þarf að uppfæra þessa tölu í ef það kemur League of Legends patch með nýrri hetju
 $data_dragon_version = '7.13.1';
 
-
-
 ## MySQL stillingar, change as needed.
 ## Athugið að það þarf að endurnefna config-sample.php í config.php
 $host = 'localhost';
 $db   = 'iceleague';
 $user = 'root';
 $pass = '';
-$charset = 'utf8';
+$charset = 'utf8mb4';
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
 $opt = [
   #Ég veit ekki nákvæmlega hvað þetta gerir, en þetta virkar fyrir mig. Pls fix.
@@ -44,6 +35,7 @@ $opt = [
   PDO::ATTR_EMULATE_PREPARES   => FALSE,
 ];
 $pdo = new PDO($dsn, $user, $pass, $opt);
+
 
 /*
  * PHP function til ad athuga hvort
@@ -54,7 +46,7 @@ $pdo = new PDO($dsn, $user, $pass, $opt);
 function kennitala($tala) {
 	// ath lengd
 	if(strlen($tala) != 10) {
-		return "Ogild kennitala";
+		return "Ogild Kennitala";
 	}
 
 	// d = dagur, m = manudur, y = ar (2 tolurstafir)
@@ -127,7 +119,7 @@ function kennitala($tala) {
 	// 19 century
 	if($century == 8) {
 		if(preg_match('/18../', $year)) {
-			return "Gild kennitala";
+			return "Gild Kennitala";
 		} else {
 			return "Ogild Kennitala";
 		}
